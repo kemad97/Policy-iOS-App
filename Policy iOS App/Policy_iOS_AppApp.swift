2b3a58733c396ef4cfbebcc4e01af46fee777171
kemad97 <90790284+kemad97@users.noreply.github.com>
2025-07-19 20:51:51 +0300

    initial commit

---
+++ Policy iOS App/Policy_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Policy_iOS_AppApp.swift
+//  Policy iOS App
+//
+//  Created by Kerolos on 19/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Policy_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
