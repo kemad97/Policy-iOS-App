e6d19aa053f8639b4751fc09bffd8b5372394f82
kemad97 <90790284+kemad97@users.noreply.github.com>
2025-07-19 21:54:04 +0300

    Update Policy_iOS_AppApp.swift

--- Policy iOS App/Policy_iOS_AppApp.swift
+++ Policy iOS App/Policy_iOS_AppApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct Policy_iOS_AppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //PolicyListView()
+            MainTabView()
         }
     }
 }
